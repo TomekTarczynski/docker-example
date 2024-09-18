47456e19e36b40b6e590c413a7599b3ce4591e41
Tomasz Tarczynski <tomek.taczynski@gmail.com>
2024-09-18 21:38:10 +0200

    Allow external access to frontend

--- flask-flask-square/frontend/frontend.py
+++ flask-flask-square/frontend/frontend.py
@@ -10,4 +10,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # Running frontend on port 5000
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Bind to 0.0.0.0 to allow external access
